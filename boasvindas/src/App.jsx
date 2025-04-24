da6ea9df9c444d12a2df5526b5e543839a8927fa
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2025-04-24 11:05:39 -0300

    24/04

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,30 @@
+import BoasVindas from "./Exemplo1"
+
+function App(){
+
+return (
+
+  <div>
+    <h1>Boas vindas ap react com Javascript</h1>
+
+    <p>Salve o Corinthians O campeão dos campeões
+Eternamente Dentro dos nossos corações
+Salve o Corinthians De tradições e glórias mil
+Tu és orgulho Dos desportistas do BrasilTeu passado é uma bandeira
+Teu presente é uma lição
+Figuras entre os primeiros
+Do nosso esporte bretãoCorinthians grande
+Sempre altaneiro
+És do Brasil O clube mais brasileiroSalve o Corinthians
+O campeão dos campeões Eternamente
+Dentro dos nossos coraçõesSalve o Corinthians De tradições e glórias mil
+Tu és orgulho Dos desportistas do Brasil
+</p>
+  <BoasVindas nome={"João"}/>
+  <BoasVindas nome={"Sarah"}/>
+  </div>
+
+  )
+}
+
+export default App
---
+++ boasvindas/src/Exemplo1.jsx
@@ -0,0 +1,11 @@
+export default function BoasVindas({nome})
+{
+    return(
+        <div>
+            <p>
+                Ola {nome} seja bem vindo ao react!!!
+            </p>
+        </div>
+    );
+
+}
