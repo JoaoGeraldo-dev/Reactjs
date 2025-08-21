e4b11471007b5030c42f927d4da5e7430844733e
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2025-08-21 09:13:22 -0300

    21/08

--- aula6/src/App.jsx
+++ aula6/src/App.jsx
@@ -3,7 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Exemplo1 from "./paginas/exemplo1";
 import Exemplo2 from "./paginas/exemplo2";
 import Home from "./paginas/Home";
-
+import Exercicio1 from "./paginas/exercicio1";
+import Exercicio2 from "./paginas/exercicio2";
+import Exercicio3 from "./paginas/exercicio3";
+import Exercicio4 from "./paginas/exercicio4";
+import Exercicio5 from "./paginas/exercicio5";
+import "./paginas/estilos.css"
 
 export default function App() {
   return (
@@ -12,6 +17,12 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/exemplo1" element={<Exemplo1 />} />
         <Route path="/exemplo2" element={<Exemplo2 />} />
+
+        <Route path="/ex1" element={<Exercicio1/>} />
+        <Route path="/ex2" element={<Exercicio2 />} />
+        <Route path="/ex3" element={<Exercicio3 />} />
+        <Route path="/ex4" element={<Exercicio4 />} />
+        <Route path="/ex5" element={<Exercicio5 />} />
       </Routes>
 
     </BrowserRouter>
--- aula6/src/paginas/Home.jsx
+++ aula6/src/paginas/Home.jsx
@@ -6,8 +6,16 @@ export default function Home() {
             <h1>Estudo de Estados React</h1>
 
             <ul>
+                <li><Link to="/exemplo1">Exemplo 1</Link></li>
+                <li><Link to="/exemplo2">Exemplo 2</Link></li>
+            </ul>
+                <h3>Exercicios</h3>
+            <ul>    
                 <li><Link to="/ex1">Exemplo 1</Link></li>
                 <li><Link to="/ex2">Exemplo 2</Link></li>
+                <li><Link to="/ex3">Exemplo 3</Link></li>
+                <li><Link to="/ex4">Exemplo 4</Link></li>
+                <li><Link to="/ex5">Exemplo 5</Link></li>
             </ul>
         </div>
     );
