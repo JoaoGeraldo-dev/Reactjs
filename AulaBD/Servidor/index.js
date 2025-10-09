fefefaeee94b2863f985ad4c4f215581675c635a
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2025-10-09 09:01:45 -0300

    09/10

---
+++ AulaBD/Servidor/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const mysql2 = require("mysql2");
+const { query } = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const banco = mysql2.createConnection({
+    host: "localhost",
+    port : 3308,
+    user : "root",
+    password : "mysqlfatec",
+    database : "aulabd"  
+});
+
+banco.connect( (erro) =>{
+
+    if(erro)
+    {
+       console.log("Erro ao conectar ao MysQL: ");
+       console.log(erro);     
+    }else
+    {
+        console.log("Conectado ao MySQL com sucesso !");
+
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando em http://localhost:" + PORT);
+});
+
+app.get("/alunos", (req, res) => {
+    const sql = "SELECT * FROM alunos";
+    banco,query(sql, (erro, resultados) => {
+        if(erro){
+            console.log(erro);
+            return res.status(500).json({error: "Erro ao consultar alunos"});
+        }else{
+            console.log(resultados);
+            return res.status(200).json(resultados);
+        }
+
+    });
+});
+
+app.get("/alunos/:codigo", (req, res) => {
+
+});
