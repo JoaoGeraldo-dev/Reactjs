9981a40f9ba73662ead9be29bbad85c507202eee
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2025-10-09 08:14:14 -0300

    09/10

---
+++ AulaBD/banco.sql
@@ -0,0 +1,14 @@
+create database aulabd;
+use aulabd;
+
+create table alunos(
+codigo int key auto_increment,
+nome varchar(50),
+cidade varchar(50),
+estado varchar(2)
+);
+
+insert into alunos(nome, cidade, estado) value ("João Geraldo", "Arceburgo",
+"MG");
+
+select * from alunos;
